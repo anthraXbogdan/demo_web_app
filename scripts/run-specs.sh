78dfc979ef6b93a48d9eccb7bd91d2df57382be3
Bogdan Enache <bogdanenache07@gmail.com>
2022-03-27 23:38:34 +0300

    Run specs on CircleCI

--- scripts/run-specs.sh
+++ scripts/run-specs.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-set -e
-function copy_artifacts {
-  mkdir -p /tmp/artifacts
-  CONTAINER_ID=$(docker-compose ps -q app)
-  docker cp ${CONTAINER_ID}:/tmp/rspec.xml /tmp/artifacts/rspec.yml
-}
-
-docker-compose up -d app
-docker-compose exec app \
-  bundle exec rspec --format progress --format RspecJunitFormatter --out /tmp/rspec.xml
-STATUS=$?
-copy_artifacts
-exit ${STATUS}
+docker-compose run app bundle exec rspec
